3c779f8138f456ee04cc848da2cdeee775d4223e
dentkwi <46320033+dentkwi@users.noreply.github.com>
2021-03-20 12:07:50 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+function Food({fav}){
+ 
+  return <h1>I like {fav}</h1>
+  
+}
+
 function App() {
   return (
     <div className="App">
      <h1 >Hello!!!!!!</h1>Hello!!
+    <Food fav="kimchi" />
+    <Food fav="ramen" />
+    <Food fav="samgiopsal" />
+    <Food fav="chukumi" />
     </div>
   );
 }
